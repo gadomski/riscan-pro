011f9c475a72807addaecca6081f75b70bd02156
Pete Gadomski <pete.gadomski@gmail.com>
2016-08-29 10:46:58 -0600

    More work on the executable

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,13 @@ const USAGE: &'static str = "
 RiSCAN PRO utilities.
 
 Usage:
-    riscan-pro colorize <infile> <project> <outfile> [--scan-position=<name>] [--allow-missing-images=<bool>]
+    riscan-pro colorize <infile> <project> <outfile> [--scan-position=<name>] [--allow-missing-images]
     riscan-pro (-h | --help)
 
 Options:
     -h --help                       Show this screen.
     --scan-position=<name>          Specify the scan position name. If not provided, we will try to deduce the scan position from the infile name.
-    --allow-missing-images=<bool>   Allow colorization even if some images are missing from the scan position [default: false].
+    --allow-missing-images          Allow colorization even if some images are missing from the scan position.
 ";
 
 #[derive(Debug, RustcDecodable)]
@@ -35,15 +35,16 @@ struct Args {
 fn main() {
     let args: Args = Docopt::new(USAGE).and_then(|d| d.decode()).unwrap_or_else(|e| e.exit());
     if args.cmd_colorize {
-        let project = Project::from_path(args.arg_project).unwrap();
-        let scan_position = if let Some(name) = args.flag_scan_position {
+        let project = Project::from_path(&args.arg_project)
+            .expect(&format!("Could not open projet file: {}", args.arg_project));
+        let scan_position = if let Some(name) = args.flag_scan_position.as_ref() {
             project.scan_position(&name).expect(&format!("Cound not find scan position: {}", name))
         } else {
             Path::new(&args.arg_infile)
                 .file_stem()
                 .and_then(|name| project.scan_position_with_scan(&name.to_string_lossy()))
                 .expect(&format!("Could not deduce scan position from file: {}",
-                                 args.arg_infile))
+                                 &args.arg_infile))
         };
         if !args.flag_allow_missing_images {
             let images = scan_position.missing_images();
@@ -52,9 +53,10 @@ fn main() {
                        images.into_iter().map(|i| i.name()).collect::<Vec<_>>().join(", "));
             }
         }
-        let reader = Reader::from_path(args.arg_infile).unwrap();
-        let mut writer = Writer::from_path(args.arg_outfile)
-            .unwrap()
+        let reader = Reader::from_path(&args.arg_infile)
+            .expect(&format!("Could not open infile: {}", &args.arg_infile));
+        let mut writer = Writer::from_path(&args.arg_outfile)
+            .expect(&format!("Could not open outfile: {}", &args.arg_outfile))
             .header(reader.header().clone())
             .point_format(PointFormat(1))
             .open()
