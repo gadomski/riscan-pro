97cd26dc863edf6b4d73a1e4c869c5deda353691
Pete Gadomski <pete.gadomski@gmail.com>
2017-02-24 16:45:43 -0700

    Redo coordiantes from scan positions

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ fn main() {
         let project = Project::from_path(args.arg_project).unwrap();
         for scan_position in project.scan_positions() {
             let glcs = scan_position.socs_to_glcs((0., 0., 0.));
-            println!("{},{:.2},{:.2},{:.2}", scan_position.name(), glcs.0, glcs.1, glcs.2);
+            println!("{:.2} {:.2}", glcs.0, glcs.1);
         }
     } else if args.cmd_export_filters {
         let scan_position = if let Some(scan_position) = args.arg_scan_position {
