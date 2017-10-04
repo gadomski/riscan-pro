2ba05f4ccd6ba1ba23cbc5af6cbb62a89632da45
Pete Gadomski <pete.gadomski@gmail.com>
2017-10-04 16:44:18 +0000

    Demonstrate opening everything

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 extern crate docopt;
+extern crate irb;
+extern crate las;
 extern crate riscan_pro;
+extern crate scanifc;
 #[macro_use]
 extern crate serde_derive;
 
 use docopt::Docopt;
+use irb::text::File;
 use riscan_pro::Project;
+use scanifc::point3d::Stream;
 use std::path::PathBuf;
 
 const USAGE: &'static str = "
@@ -30,7 +35,19 @@ fn main() {
         .and_then(|d| d.deserialize())
         .unwrap_or_else(|e| e.exit());
     if args.cmd_colorize {
-        let project = Project::from_path(args.arg_project).unwrap();
-        unimplemented!()
+        colorize(&args);
     }
+}
+
+fn colorize(args: &Args) {
+    let project = Project::from_path(&args.arg_project).expect("Could not open project");
+    let stream = Stream::from_path(&args.arg_rxp).open().expect(
+        "Could not open stream",
+    );
+    let image = File::open(&args.arg_image)
+        .expect("Could not open image")
+        .into_image()
+        .expect("Could not read image");
+    for point in stream {}
+    unimplemented!()
 }
