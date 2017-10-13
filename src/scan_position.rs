2eca598696dddc12807fc636cd025f6f534af597
Pete Gadomski <pete.gadomski@gmail.com>
2017-10-13 09:42:14 -0600

    Add calibration accessors to Image

--- src/scan_position.rs
+++ src/scan_position.rs
@@ -1,6 +1,6 @@
 //! Scan positions and their consituant parts.
 
-use Result;
+use {CameraCalibration, Error, MountCalibration, Project, Result};
 use nalgebra::Projective3;
 use std::collections::HashMap;
 use std::path::Path;
@@ -50,5 +50,61 @@ impl ScanPosition {
             .map(|file_stem| file_stem.to_string_lossy())
             .and_then(|file_stem| self.images.get(file_stem.as_ref()))
             .ok_or(Error::ImageFromPath(path.as_ref().to_path_buf()))
+    }
+}
+
+impl Image {
+    /// Finds and returns this image's camera calibration.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use riscan_pro::Project;
+    /// let project = Project::from_path("data/Project.RiSCAN").unwrap();
+    /// let mut image = project.scan_positions
+    ///     .get("SP01")
+    ///     .unwrap()
+    ///     .images
+    ///     .get("SP01 - Image001")
+    ///     .unwrap()
+    ///     .clone();
+    /// image.camera_calibration(&project).unwrap();
+    /// image.camera_calibration_name = "Not a camera calibration".to_string();
+    /// assert!(image.camera_calibration(&project).is_err());
+    /// ```
+    pub fn camera_calibration<'a>(&self, project: &'a Project) -> Result<&'a CameraCalibration> {
+        project
+            .camera_calibrations
+            .get(&self.camera_calibration_name)
+            .ok_or(Error::MissingCameraCalibration(
+                self.camera_calibration_name.clone(),
+            ))
+    }
+
+    /// Finds and returns this image's mount calibration.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use riscan_pro::Project;
+    /// let project = Project::from_path("data/Project.RiSCAN").unwrap();
+    /// let mut image = project.scan_positions
+    ///     .get("SP01")
+    ///     .unwrap()
+    ///     .images
+    ///     .get("SP01 - Image001")
+    ///     .unwrap()
+    ///     .clone();
+    /// image.mount_calibration(&project).unwrap();
+    /// image.mount_calibration_name = "Not a mount calibration".to_string();
+    /// assert!(image.mount_calibration(&project).is_err());
+    /// ```
+    pub fn mount_calibration<'a>(&self, project: &'a Project) -> Result<&'a MountCalibration> {
+        project
+            .mount_calibrations
+            .get(&self.mount_calibration_name)
+            .ok_or(Error::MissingMountCalibration(
+                self.mount_calibration_name.clone(),
+            ))
     }
 }
