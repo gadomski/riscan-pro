5df31f986c69dda0ee8770719b56f9e04a1d7300
Pete Gadomski <pete.gadomski@gmail.com>
2018-01-29 13:06:43 -0700

    Conver to yaml for clap

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,14 @@ extern crate clap;
 extern crate riscan_pro;
 extern crate serde_json;
 
-use clap::{App, Arg, SubCommand};
+use clap::App;
 use riscan_pro::Project;
 
 fn main() {
-    let matches = App::new("RiSCAN Pro")
-        .author("Pete Gadomski <pete@gadom.ski>")
-        .about("Query RiSCAN Pro projects")
-        .subcommand(
-            SubCommand::with_name("info")
-                .about("Show info about the project")
-                .version(crate_version!())
-                .arg(Arg::with_name("PROJECT").index(1).required(true).help(
-                    "Path to the project",
-                ))
-                .arg(Arg::with_name("compact").long("compact").short("c").help(
-                    "Prints compact JSON",
-                )),
-        )
-        .get_matches();
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).version(crate_version!()).get_matches();
 
-    if let Some(matches) = matches.subcommand_matches("info") {
+    if let Some(matches) = matches.subcommand_matches("json") {
         let path = matches.value_of("PROJECT").unwrap();
         let project = Project::from_path(path).expect("Unable to create project");
         let json = if matches.is_present("compact") {
